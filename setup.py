a73b2ed422d8d59706f782166edaceb85536921a
Phil Elson <pelson.pub@gmail.com>
2017-07-04 09:11:30 +0100

    Added the project template, including CI, SCOPE and a setup.py.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(
+    name='pyggybank',
+    description='Interface to your internet banking',
+    version='0.1',
+    author='Phil Elson',
+    author_email='pelson.pub@gmail.com',
+    url='https://github.com/pelson/pyggybank',
+    license='BSD',
+    packages=[
+        'pyggybank',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ]
+)
